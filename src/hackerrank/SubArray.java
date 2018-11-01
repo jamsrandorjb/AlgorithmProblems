1eda00d6c35022327d73414654746b9f4a691fec
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 19:24:47 -0400

    subArray problem on hackerrank

---
+++ src/hackerrank/SubArray.java
@@ -0,0 +1,37 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/java-negative-subarray/problem
+ * solved by jama
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class SubArray {
+
+	public static void main(String[] args) {
+		/*
+		 * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+		 * class should be named Solution.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		int counter = 0;
+		for (int k = 0; k < n; k++)
+			a[k] = sc.nextInt();
+
+		for (int j = 0; j < n; j++) {
+			int sum = 0;
+			for (int k = j; k < n; k++) {
+				sum = a[k] + sum;
+				if (sum < 0) {
+					counter++;
+				}
+			}
+		}
+		sc.close();
+		System.out.println(counter);
+	}
+}
