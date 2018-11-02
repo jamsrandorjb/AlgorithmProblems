1a6f5d561f0eb19a879b9d90cdfd9cd4c510e598
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:37:20 -0400

    gradingStudents problem on hackerrank

---
+++ src/hackerrank/GradingStudents.java
@@ -0,0 +1,69 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/grading/problem
+ * solved by jama
+ */
+
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class GradingStudents {
+
+    /*
+     * Complete the gradingStudents function below.
+     */
+    static int[] gradingStudents(int[] grades) {
+        int[] result = new int[grades.length];
+        outerloop:
+        for(int i = 0; i<grades.length; i++)
+        {
+            int temp = grades[i];
+            result[i] = temp; 
+            if(temp < 38) continue;
+            
+            for(int j=0; j<=2; j++)
+            {
+                if((temp + j) % 5 == 0)
+                {
+                    result[i] = temp+j;
+                    continue outerloop;
+                }
+            }         
+        }
+        return result;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[] grades = new int[n];
+
+        for (int gradesItr = 0; gradesItr < n; gradesItr++) {
+            int gradesItem = Integer.parseInt(scan.nextLine().trim());
+            grades[gradesItr] = gradesItem;
+        }
+
+        int[] result = gradingStudents(grades);
+
+        for (int resultItr = 0; resultItr < result.length; resultItr++) {
+            bw.write(String.valueOf(result[resultItr]));
+
+            if (resultItr != result.length - 1) {
+                bw.write("\n");
+            }
+        }
+
+        bw.newLine();
+
+        bw.close();
+    }
+}
