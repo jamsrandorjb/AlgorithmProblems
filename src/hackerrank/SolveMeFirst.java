d837f996ceed9cf4f030217fa7ff89195e02577e
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:11:32 -0400

    solve me first problem on hackerrank

---
+++ src/hackerrank/SolveMeFirst.java
@@ -0,0 +1,32 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/solve-me-first/problem
+ * solved by jama
+ */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SolveMeFirst {
+
+    static int solveMeFirst(int a, int b) {
+         // Hint: Type return a+b; below 
+        return a+b;
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
