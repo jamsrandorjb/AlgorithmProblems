730ffeab9b14b83f7b914ad30337f57362f7b76b
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:31:38 -0400

    Staircase problem on hackerrank

---
+++ src/hackerrank/Staircase.java
@@ -0,0 +1,28 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/staircase/problem
+ * solved by jama
+ */
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Staircase {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int j=0;j<n;j++){
+            for(int i=1;i<=n;i++){
+                System.out.print(i<n-j?" ":"#");
+            }
+            System.out.println("");
+        }
+    }
+}
+
