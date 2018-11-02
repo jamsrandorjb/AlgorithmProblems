53802eed9a883aef5a93bfa07e11948f129f2c21
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 21:13:34 -0400

    CountingSortPart1 problem on hackerrank

---
+++ src/hackerrank/CountingSortPart1.java
@@ -0,0 +1,63 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/countingsort1/problem
+ * hackerrank profile url:  https://www.hackerrank.com/jamsrandorjb?hr_r=1
+ * solved by jama
+ */
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class CountingSortPart1 {
+
+    // Complete the countingSort function below.
+    static int[] countingSort(int[] arr) {
+    int[] result = new int[100];
+    for(int i : arr)
+        result[i]++;
+    return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = countingSort(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
