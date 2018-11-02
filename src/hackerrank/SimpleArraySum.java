712ee8f3b01455931b4a1c7cec2e4cd38a611025
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:13:13 -0400

    simple array sum problem

---
+++ src/hackerrank/SimpleArraySum.java
@@ -0,0 +1,36 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/simple-array-sum/problem
+ * solved by jama
+ */
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SimpleArraySum {
+
+    static int simpleArraySum(int n, int[] ar) {
+        // Complete this function
+        int sum = 0;
+        for(int i=0; i < ar.length; i++)
+           sum = sum + ar[i];
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = simpleArraySum(n, ar);
+        System.out.println(result);
+    }
+}
+
