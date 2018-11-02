aaa52e3802fb13fd707594a5b5415422f8476521
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:17:17 -0400

    diagonal difference problem on hackerrank

---
+++ src/hackerrank/DiagonalDifference.java
@@ -0,0 +1,59 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/diagonal-difference/problem
+ * solved by jama
+ */
+
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class DiagonalDifference {
+
+    /*
+     * Complete the diagonalDifference function below.
+     */
+    static int diagonalDifference(int[][] a) {
+        /*
+         * Write your code here.
+         */
+        int temp1 = 0;
+        int temp2 = 0;
+        for(int i =0; i < a.length; i++){
+            temp1 += a[i][i];
+            temp2 += a[i][a.length-1-i];
+        }
+        return temp1 - temp2 < 0 ? -1*(temp1-temp2) : temp1-temp2;
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[][] a = new int[n][n];
+
+        for (int aRowItr = 0; aRowItr < n; aRowItr++) {
+            String[] aRowItems = scan.nextLine().split(" ");
+
+            for (int aColumnItr = 0; aColumnItr < n; aColumnItr++) {
+                int aItem = Integer.parseInt(aRowItems[aColumnItr].trim());
+                a[aRowItr][aColumnItr] = aItem;
+            }
+        }
+
+        int result = diagonalDifference(a);
+
+        bw.write(String.valueOf(result));
+        bw.newLine();
+
+        bw.close();
+    }
+}
