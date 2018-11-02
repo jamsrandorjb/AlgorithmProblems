25bbea1d93a2dd0d34d4bbc476e5fe3ac5db3233
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 21:10:33 -0400

    RunningTimeofAlgorithms problem on Hackerrank

---
+++ src/hackerrank/RunningTimeOfAlgorithms.java
@@ -0,0 +1,69 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/runningtime/problem
+ * hackerrank profile url:  https://www.hackerrank.com/jamsrandorjb?hr_r=1
+ * solved by jama
+ */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class RunningTimeOfAlgorithms {
+
+    // Complete the runningTime function below.
+    static int runningTime(int[] arr) {
+        int counter=0;
+        for (int i=1; i<arr.length; ++i)
+        {
+            int key = arr[i];
+            int j = i-1;
+ 
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j>=0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];
+                counter++;
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
+        return counter;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
