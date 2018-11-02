d17d407e71b955ebaa6b538402d333f914559fae
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 21:21:22 -0400

    Game of stones problem on hackerrank

---
+++ src/hackerrank/GameOfStones.java
@@ -0,0 +1,47 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/game-of-stones-1/problem
+ * hackerrank profile url:  https://www.hackerrank.com/jamsrandorjb?hr_r=1
+ * solved by jama
+ */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class GameOfStones {
+
+    // Complete the gameOfStones function below.
+    static String gameOfStones(int n) {
+        if( n%7==1 || n%7==0) return "Second";
+        else return "First";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String result = gameOfStones(n);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
