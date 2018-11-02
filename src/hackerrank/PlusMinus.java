3da6689a1d4bc93de455847a5000d8b57dcc9af6
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:29:59 -0400

    plusMinus problem on hackerrank

---
+++ src/hackerrank/PlusMinus.java
@@ -0,0 +1,39 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/plus-minus/problem
+ * solved by jama
+ */
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class PlusMinus {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        double negativeCount=0.0,positiveCount=0.0,zeroCount=0.0;
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+            int num = arr[arr_i];
+            if(num<0){
+                negativeCount++;
+            }else if(num>0){
+                positiveCount++;
+            }else{
+                zeroCount++;
+            }
+        }
+        System.out.printf("%.6f\n%.6f\n%.6f",positiveCount/n,negativeCount/n,zeroCount/n);
+    }
+}
+
+
+
