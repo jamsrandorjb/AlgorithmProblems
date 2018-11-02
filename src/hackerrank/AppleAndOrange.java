398aaa60a6f305e392a264c8156fd3613ecb09d2
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:38:32 -0400

    Apple and Orange problem on hackerrank

---
+++ src/hackerrank/AppleAndOrange.java
@@ -0,0 +1,82 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/apple-and-orange/problem
+ * solved by jama
+ */
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class AppleAndOrange {
+
+    /*
+     * Complete the countApplesAndOranges function below.
+     */
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        /*
+         * Write your code here.
+         */
+        int applesCount = 0;
+        int orangesCount = 0;
+        for(int i=0; i<apples.length; i++)
+            if(a+apples[i] >= s && a+apples[i] <= t)
+                applesCount++;
+        for(int i=0; i<oranges.length; i++)
+            if(b+oranges[i] >= s && b+oranges[i] <= t)
+                orangesCount++;
+        
+        
+        System.out.println(applesCount);
+        System.out.println(orangesCount);
+        
+        
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scan.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0].trim());
+
+        int t = Integer.parseInt(st[1].trim());
+
+        String[] ab = scan.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0].trim());
+
+        int b = Integer.parseInt(ab[1].trim());
+
+        String[] mn = scan.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0].trim());
+
+        int n = Integer.parseInt(mn[1].trim());
+
+        int[] apple = new int[m];
+
+        String[] appleItems = scan.nextLine().split(" ");
+
+        for (int appleItr = 0; appleItr < m; appleItr++) {
+            int appleItem = Integer.parseInt(appleItems[appleItr].trim());
+            apple[appleItr] = appleItem;
+        }
+
+        int[] orange = new int[n];
+
+        String[] orangeItems = scan.nextLine().split(" ");
+
+        for (int orangeItr = 0; orangeItr < n; orangeItr++) {
+            int orangeItem = Integer.parseInt(orangeItems[orangeItr].trim());
+            orange[orangeItr] = orangeItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apple, orange);
+    }
+}
+
