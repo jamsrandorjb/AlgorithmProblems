83ca7d975f37702828295ca7c2715829e49aee72
jamsrandorjb <jamsrandorj1@gmail.com>
2018-11-01 20:50:56 -0400

    Bon Appetit problem on Hackerrank

---
+++ src/hackerrank/BonAppetit.java
@@ -0,0 +1,39 @@
+package hackerrank;
+
+/**
+ * problem url: https://www.hackerrank.com/challenges/bon-appetit/problem
+ * solved by jama
+ */
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class BonAppetit {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] list = scanner.nextLine().split(" ");
+
+        String[] last = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(last[0]);
+        int sum =0;
+        for(int i =0; i<list.length; i++){
+            if(t!=i)
+                sum+=Integer.parseInt(list[i]);     
+        }
+        System.out.println(m-sum/2 == 0 ? "Bon Appetit" :  m-sum/2);
+        scanner.close();
+
+    }
+}
