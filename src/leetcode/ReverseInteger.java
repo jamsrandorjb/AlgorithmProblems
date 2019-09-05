fd732c62db92ca1e029569ac5f23e5bfa64a3579
Jamsrandorj B <jamsrandorj1@gmail.com>
2019-09-04 22:04:50 -0400

    checked the odd cases in the ReverseInteger

--- src/leetcode/ReverseInteger.java
+++ src/leetcode/ReverseInteger.java
@@ -14,28 +14,22 @@ public class ReverseInteger {
 	}
 	
 	public static int reverse(int x) {
-		
-		boolean checkNegative = false;
 		int result = 0;
 		
-		//checking if the x is negative
-		//if yes we have to do some calculations here and there
-		if(x < 0) {
-			checkNegative = true;
-			x = 0 - x;
-		}
-		
 		while(x != 0) {
-			result = result * 10 + x % 10;
-			x = x / 10;
+            int pop = x % 10;
+            x = x / 10;
+            
+            if (result > Integer.MAX_VALUE/10 || ( result == Integer.MAX_VALUE / 10 && pop > 7 )) {
+			    return 0;
+		    }
+            if (result < Integer.MIN_VALUE/10 || ( result == Integer.MIN_VALUE / 10 && pop < -8)) {
+			    return 0;
+		    }
+            
+			result = result * 10 + pop;
 		}
-		
-		if(checkNegative) result = 0 - result;
-		
-		if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
-			return 0;
-		}
-		
+        
         return result;
     }
 }
