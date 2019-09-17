1f2219a2a5b1a27adfe10185aa9f527bdcbc520c
Jamsrandorj B <jamsrandorj1@gmail.com>
2019-09-17 00:17:31 -0400

    Leetcode: Best Time to Buy and Sell Stocl problem

---
+++ src/leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/*
+* Best Time to Buy and Sell Stock 
+* problem url: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+* leetcode profile url:  https://leetcode.com/jamsrandorj/
+* solved by jama
+*/
+public class BestTimeToBuyAndSellStock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(maxProfit(new int[] {7,1,5,3,6,4}));
+	}
+	
+	public static int maxProfit(int[] prices) {
+		//declaring max profit and minValue
+        int maxProfit = 0;
+        int minValue = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < prices.length; i++){
+        	//if the existing minValue is bigger than i th element of the array, we have to update the minValue
+            if(minValue >= prices[i]){
+                minValue = prices[i]; 
+                continue;
+            }
+            //if the i th element is bigger than minValue we can calculate the profit
+            int profit = prices[i] - minValue;
+            //checking the maxProfit
+            maxProfit =  profit > maxProfit ? profit : maxProfit;       
+        }
+        
+        //returning the maxProfit
+        return maxProfit;
+    }
+}
