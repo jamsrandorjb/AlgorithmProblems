dda821ed9dbcd3a58bebee4e97671cc0d9fbec2c
Jamsrandorj B <jamsrandorj1@gmail.com>
2019-10-10 10:49:01 -0400

    added sub package for the tree

---
+++ src/leetcode/tree/RangeSumBST.java
@@ -0,0 +1,29 @@
+package leetcode.tree;
+
+/*
+* Range Sum BST
+* problem url: https://leetcode.com/problems/range-sum-of-bst/
+* leetcode profile url:  https://leetcode.com/jamsrandorj/
+* solved by jama
+*/
+public class RangeSumBST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(rangeSumBST(new TreeNode(5), 5, 10));
+	}
+	
+	public static int rangeSumBST(TreeNode root, int L, int R) {
+        int sum = 0;
+        if(root == null) return sum;
+        
+        if(root.val >= L && root.val <= R)
+            sum += root.val;
+            
+        int leftSum = rangeSumBST(root.left, L, R);
+        int rightSum = rangeSumBST(root.right, L, R);
+        
+        
+        return sum + leftSum + rightSum;
+    }
+}
