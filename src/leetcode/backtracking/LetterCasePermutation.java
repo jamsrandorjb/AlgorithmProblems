ae1fc3d65789ba25301d711353dffe3e19afdf79
Jamsrandorj B <jamsrandorj1@gmail.com>
2019-11-19 09:33:08 -0500

    LeetCode: Letter Case Permutation

---
+++ src/leetcode/backtracking/LetterCasePermutation.java
@@ -0,0 +1,43 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* Letter Case Pemutation
+* problem url: https://leetcode.com/problems/letter-case-permutation/
+* leetcode profile url:  https://leetcode.com/jamsrandorj/
+*/
+public class LetterCasePermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(String caseString: letterCasePermutation("a1b2"))
+			System.out.println(caseString);
+	}
+	
+	       
+	public static List<String> letterCasePermutation(String S) {
+	    List<String> casePemutations = new ArrayList<>();
+	    letterCasePermutationRecursice(casePemutations, S.toCharArray(), 0);
+	    return casePemutations;
+	}
+	
+	public static void letterCasePermutationRecursice(List<String> casePemutations, char[] chars, int start) {
+		casePemutations.add(new String(chars));
+		for (int i = start; i < chars.length; i++) {
+			if (!(chars[i] >= 'a' && chars[i] <= 'z' || chars[i] >= 'A' && chars[i] <= 'Z')) continue;
+			switchLetterCase(chars, i);
+			letterCasePermutationRecursice(casePemutations, chars, i + 1);
+			switchLetterCase(chars, i);
+		}
+	}
+	
+	public static void switchLetterCase(char[] chars, int index) {
+	    char c = chars[index];
+	    if (c >= 'a' && c <= 'z') chars[index] -= 32;
+	    else if (c >= 'A' && c <= 'Z') chars[index] += 32;
+	}           
+
+
+}
