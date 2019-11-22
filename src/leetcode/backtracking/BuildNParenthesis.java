0ff09f395f5dd5f5b88cc80f7f4353762f3ec6f4
Jamsrandorj B <jamsrandorj1@gmail.com>
2019-11-22 00:44:59 -0500

    Create BuildNParenthesis.java

---
+++ src/leetcode/backtracking/BuildNParenthesis.java
@@ -0,0 +1,35 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+* 22. Generate Parentheses
+* problem url: https://leetcode.com/problems/generate-parentheses/
+* leetcode profile url:  https://leetcode.com/jamsrandorj/
+ * solved by jama
+*/
+public class BuildNParenthesis {
+	public static void main(String[] args) { 
+		System.out.println(generateParenthesis(3));	
+	}
+	
+	public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        buildParenthesis(result, n, n, "");
+        
+        return result;
+    }
+    
+    private static void buildParenthesis(List<String> result, int left, int right, String built){
+        if(left == 0 && right == 0){
+            result.add(built);
+            return;
+        }
+        if(left > 0) buildParenthesis(result, left-1, right, built+"(");
+        if (right > left) buildParenthesis(result, left, right-1, built+")");
+    }
+}
+
+
