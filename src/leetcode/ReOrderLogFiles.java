b3ce07869f43dc5752fa6d92fd7d5c6a2d1558f6
Jamsrandorj B <jamsrandorj1@gmail.com>
2019-12-04 18:01:16 -0500

    Update ReOrderLogFiles.java

--- src/leetcode/ReOrderLogFiles.java
+++ src/leetcode/ReOrderLogFiles.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /*
 * Find Pivot Index
@@ -12,9 +14,54 @@ public class ReOrderLogFiles {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(String log : reorderLogFiles(new String[] {"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"}))
+		for(String log : reorderLogFiles1(new String[] {"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"}))
 			System.out.println(log);
 	}
+	public static String[] reorderLogFiles1(String[] logs) {
+        PriorityQueue<String> heap = new PriorityQueue<String>(new Comparator<String>(){
+            public int compare(String s1, String s2){
+                String firstElementofLog1 = s1.split(" ")[1];
+                String firstElementofLog2 = s2.split(" ")[1];
+                try{
+                    //both of the items are number
+                    int item1 = Integer.parseInt(firstElementofLog1);
+                    int item2 = Integer.parseInt(firstElementofLog2);
+                    return item2 - item1;
+                }
+                catch(Exception e){
+                    try{
+                       //item1 is number only 
+                       int item1 = Integer.parseInt(firstElementofLog1); 
+                       return 1;
+                    }
+                    catch(Exception e1){
+                        try{
+                           //item2 is number only
+                           int item2 = Integer.parseInt(firstElementofLog2);
+                           return -1;  
+                        }
+                        catch(Exception e2){
+                            //both logs are string
+                            return firstElementofLog1.compareTo(firstElementofLog2);
+                        }
+                    }
+                }
+            }
+        });
+        
+        
+        for(int i = 0; i < logs.length; i++ ){
+            heap.offer(logs[i]);
+        }
+        
+        for(int i = 0; i < logs.length; i++)
+        {
+            logs[i] = heap.poll();
+        }
+        
+        return logs;
+    }
+	
 	
 	public static String[] reorderLogFiles(String[] logs) {
         ArrayList<String> stringLogs = new ArrayList<String>();
