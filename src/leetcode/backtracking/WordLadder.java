7e911df9c3101229f70d4713874ff807a642b0f4
Jamsrandorj B <jamsrandorj1@gmail.com>
2020-03-23 01:23:09 -0400

    Update WordLadder.java

--- src/leetcode/backtracking/WordLadder.java
+++ src/leetcode/backtracking/WordLadder.java
@@ -22,40 +22,43 @@ public class WordLadder {
 		arr.add("cog");
 		System.out.println(ladderLength("hit", "cog", arr));
 	}
-    
+
 	public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> hSet = new HashSet<>();
-        for(String s : wordList) hSet.add(s);
-        
-        if(!hSet.contains(endWord)) return 0;
-        
-        int[] minPath = new int[1];
-        minPath[0] = Integer.MAX_VALUE;
-        dfs(beginWord, endWord, hSet, new ArrayList<String>(), minPath);
-        
-        return minPath[0] == Integer.MAX_VALUE ? 0 : minPath[0];  
-    }
-    
-    private static void dfs(String beginWord, String endWord, HashSet<String> wordSet, List<String> tempResult, int[] minPath){
-        tempResult.add(beginWord);
-        if(beginWord.equals(endWord)) {
-            minPath[0] = minPath[0] > tempResult.size() ? tempResult.size() : minPath[0];
-        }
-        else{
-        	char[] arr = beginWord.toCharArray();
-        	for(int i = 0; i < arr.length; i++){
-            	char c1 = arr[i];
-                for(char c = 'a'; c <= 'z'; ++c){
-                    if(beginWord.charAt(i) == c) continue;
-                    arr[i] = c;
-                    String mutatedWord = new String(arr);
-                    if(wordSet.contains(mutatedWord) && !tempResult.contains(mutatedWord)){
-                        dfs(mutatedWord, endWord, wordSet, tempResult, minPath);
-                    }
-                    arr[i] = c1;
-                }
-            }    
-        }
-        tempResult.remove(beginWord);
-    }
+		HashSet<String> hSet = new HashSet<>();
+		for (String s : wordList)
+			hSet.add(s);
+
+		if (!hSet.contains(endWord))
+			return 0;
+
+		int[] minPath = new int[1];
+		minPath[0] = Integer.MAX_VALUE;
+		dfs(beginWord, endWord, hSet, new ArrayList<String>(), minPath);
+
+		return minPath[0] == Integer.MAX_VALUE ? 0 : minPath[0];
+	}
+
+	private static void dfs(String beginWord, String endWord, HashSet<String> wordSet, List<String> tempResult,
+			int[] minPath) {
+		tempResult.add(beginWord);
+		if (beginWord.equals(endWord)) {
+			minPath[0] = Math.min(minPath[0], tempResult.size());
+		} else {
+			char[] arr = beginWord.toCharArray();
+			for (int i = 0; i < arr.length; i++) {
+				char c1 = arr[i];
+				for (char c = 'a'; c <= 'z'; ++c) {
+					if (beginWord.charAt(i) == c)
+						continue;
+					arr[i] = c;
+					String mutatedWord = new String(arr);
+					if (wordSet.contains(mutatedWord) && !tempResult.contains(mutatedWord)) {
+						dfs(mutatedWord, endWord, wordSet, tempResult, minPath);
+					}
+					arr[i] = c1;
+				}
+			}
+		}
+		tempResult.remove(beginWord);
+	}
 }
