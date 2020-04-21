0fb59f23ea6b0a5a81bd09e4fa9fbfd8f6bc6e43
Jamsrandorj B <jamsrandorj1@gmail.com>
2020-04-21 15:44:25 -0400

    Create DungeonGames.java

---
+++ src/leetcode/dynamicProgramming/DungeonGames.java
@@ -0,0 +1,39 @@
+package leetcode.dynamicProgramming;
+
+/*
+ * 174. Dungeon Game
+ * problem url: https://leetcode.com/problems/dungeon-game/
+ * leetcode profile url: https://leetcode.com/jamsrandorj/ 
+ * solved by jama  
+ */
+public class DungeonGames {
+	public static void main(String[] args) {
+		System.out.println(calculateMinimumHP(new int[][] { { -2, -3, 3 }, { -5, -10, 1 }, { 10, 30, -5 } }));
+	}
+
+	public static int calculateMinimumHP(int[][] dungeon) {
+		if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0)
+			return 1;
+
+		int N = dungeon.length;
+		int M = dungeon[0].length;
+
+		int[][] dp = new int[N][M];
+
+		dp[N - 1][M - 1] = 1 - dungeon[N - 1][M - 1];
+		dp[N - 1][M - 1] = dp[N - 1][M - 1] <= 0 ? 1 : dp[N - 1][M - 1];
+
+		for (int i = N - 1; i >= 0; --i) {
+			for (int j = M - 1; j >= 0; --j) {
+				if (i == N - 1 && j == M - 1)
+					continue;
+				int HP_D = i + 1 == N ? Integer.MAX_VALUE : dp[i + 1][j] - dungeon[i][j];
+				int HP_R = j + 1 == M ? Integer.MAX_VALUE : dp[i][j + 1] - dungeon[i][j];
+				int HP = Math.min(HP_D, HP_R);
+				dp[i][j] = HP <= 0 ? 1 : HP;
+			}
+		}
+
+		return dp[0][0];
+	}
+}
